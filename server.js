8b18cd896192d21ed5c9808127c8bed4bd63b0c2
jessicasi <jessicasi@byui.edu>
2021-03-08 13:43:50 -0800

    Start up files

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({connectionString: connectionString});
+
+var sql = "SELECT * FROM some_table_here";
+
+pool.query(sql, function(err, result) {
+    // If an error occurred...
+    if (err) {
+        console.log("Error in query: ")
+        console.log(err);
+    }
+
+    // Log this to the console for debugging purposes.
+    console.log("Back from DB with result:");
+    console.log(result.rows);
+
+
+});     
