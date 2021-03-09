fe9b04135108afc8a10795a2bc5c520a61084ab9
jessicasi <jessicasi@byui.edu>
2021-03-09 14:14:09 -0800

    get up and ready

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/server'))
+app.get('/', (req, res) => res.render('pages/index'))
 
 
-var sql = "SELECT * FROM public.testing";
+/* var sql = "SELECT * FROM public.testing";
 
 pool.query(sql, function(err, result) {
     // If an error occurred...
@@ -27,4 +27,4 @@ pool.query(sql, function(err, result) {
     console.log(result.rows);
 
 
-});     
+});      */
