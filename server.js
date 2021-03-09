ef49be0a16ec75d4b645ef5741367f8657e0c50c
jessicasi <jessicasi@byui.edu>
2021-03-09 13:39:37 -0800

    start on app

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
-
+const express = require('express')
 require('dotenv').config();
 
 const connectionString = process.env.DATABASE_URL;
-
 const pool = new Pool({connectionString: connectionString});
-console.log(process.env.DATABASE_URL);
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/server'))
+
+
 var sql = "SELECT * FROM public.testing";
 
 pool.query(sql, function(err, result) {
