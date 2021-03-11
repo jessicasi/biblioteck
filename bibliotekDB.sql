351c470f6562de20a2ad099b40fc34a6fd6f0957
jessicasi <jessicasi@byui.edu>
2021-03-11 14:41:17 -0800

    sql retrieval workiong

--- bibliotekDB.sql
+++ bibliotekDB.sql
@@ -1,132 +1,87 @@
-CREATE TABLE movie 
-( 
+CREATE TABLE movie (
     movie_id serial NOT NULL Primary Key,
     movie_name VARCHAR(75) NOT NULL,
     movie_summary TEXT
 );
-
-
 ALTER TABLE movie
-
-    ADD COLUMN genre_id INT,
+ADD COLUMN genre_id INT,
     ADD COLUMN image_id INT,
     ADD COLUMN series_id INT,
     ADD COLUMN review_id INT,
     ADD COLUMN location_id INT;
-
 ALTER TABLE movie
 ADD CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) references genre(genre_id),
-ADD CONSTRAINT fk_image_id FOREIGN KEY (image_id) references image(image_id),
-ADD CONSTRAINT fk_series_id FOREIGN KEY (series_id) references series(series_id),
-ADD CONSTRAINT fk_review_id FOREIGN KEY (review_id) references review(review_id),
-ADD CONSTRAINT fk_location_id FOREIGN KEY (location_id) references location(location_id);
-
-
-
-CREATE TABLE book
-(
+    ADD CONSTRAINT fk_image_id FOREIGN KEY (image_id) references image(image_id),
+    ADD CONSTRAINT fk_series_id FOREIGN KEY (series_id) references series(series_id),
+    ADD CONSTRAINT fk_review_id FOREIGN KEY (review_id) references review(review_id),
+    ADD CONSTRAINT fk_location_id FOREIGN KEY (location_id) references location(location_id);
+CREATE TABLE book (
     book_id serial NOT NULL Primary Key,
     book_name VARCHAR(75) NOT NULL,
     book_format VARCHAR(20) NOT NULL,
     book_summary TEXT
-  
 );
-
 ALTER TABLE book
-    ADD COLUMN image_id INT,
+ADD COLUMN image_id INT,
     ADD COLUMN series_id INT,
     ADD COLUMN review_id INT,
     ADD COLUMN location_id INT;
-
 ALTER TABLE book
-  ADD CONSTRAINT fk_series_id FOREIGN KEY (series_id) references series(series_id),
+ADD CONSTRAINT fk_series_id FOREIGN KEY (series_id) references series(series_id),
     ADD CONSTRAINT fk_review_id FOREIGN KEY (review_id) references review(review_id),
     ADD CONSTRAINT fk_location_id FOREIGN KEY (location_id) references location(location_id);
-
-CREATE TABLE location
-(
+CREATE TABLE location (
     location_id serial NOT NULL Primary Key,
     location_name VARCHAR(50) NOT NULL
 );
-
-CREATE TABLE series 
-(
+CREATE TABLE series (
     series_id serial NOT NULL Primary Key,
     series_name VARCHAR(100) NOT NULL,
     series_status boolean NOT NULL
-
 );
-
 ALTER TABLE series
-
-    ADD COLUMN review_id INT;
-
-    ALTER TABLE series
+ADD COLUMN review_id INT;
+ALTER TABLE series
 ADD CONSTRAINT fk_review_id FOREIGN KEY (review_id) references review(review_id);
-
-
-CREATE TABLE book_author
-(
-    
-);
-
+CREATE TABLE book_author ();
 ALTER TABLE book_author
-
-    ADD COLUMN book_id INT,
+ADD COLUMN book_id INT,
     ADD COLUMN author_id INT;
-
 ALTER TABLE book_author
-
 ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) references book(book_id),
     ADD CONSTRAINT fk_author_id FOREIGN KEY (author_id) references author(author_id);
-
-
-
 book_id INT references book.book_id,
-    author_id INT references author.author_id
-
-CREATE TABLE movie_book_genre
-(
-    
-);
-
+author_id INT references author.author_id CREATE TABLE movie_book_genre ();
 ALTER TABLE movie_book_genre
-
-    ADD COLUMN movie_id INT,
+ADD COLUMN movie_id INT,
     ADD COLUMN book_id INT,
     ADD COLUMN genre_id INT;
-
-
 ALTER TABLE movie_book_genre
-
 ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) references book(book_id),
-ADD CONSTRAINT fk_movie_id FOREIGN KEY (movie_id) references movie(movie_id),
-ADD CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) references genre(genre_id);
-
-
-
-
-CREATE TABLE author
-(
+    ADD CONSTRAINT fk_movie_id FOREIGN KEY (movie_id) references movie(movie_id),
+    ADD CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) references genre(genre_id);
+CREATE TABLE author (
     author_id serial NOT NULL Primary Key,
     author_name VARCHAR(75)
 );
-
-CREATE TABLE review
-(
+CREATE TABLE review (
     review_id serial NOT NULL Primary Key,
     review_text TEXT NOT NULL
 );
-
-
-CREATE TABLE genre
-(
+CREATE TABLE genre (
     genre_id serial NOT NULL Primary Key,
     genre_name VARCHAR(25) NOT NULL
 );
-
 CREATE TABLE image (
     image_id serial NOT NULL Primary Key,
     image_name VARCHAR(100) NOT NULL,
     image_path VARCHAR(100) NOT NULL
 );
+SELECT b.book_name,
+    b.author_id,
+    b.genre_id,
+    a.author_id,
+    a.author_name
+FROM book b JOIN author a ON b.author_id = a.author_id
+WHERE b.genre_id = $1::int
+
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const { BADHINTS } = require('dns');
+const {
+    BADHINTS
+} = require('dns');
 const express = require('express')
 const path = require('path')
-const { Pool } = require('pg');
-//const { callbackify } = require('util');
+const {
+    Pool
+} = require('pg');
 const PORT = process.env.PORT || 5000
 require('dotenv').config();
 
 const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({connectionString: connectionString});
+const pool = new Pool({
+    connectionString: connectionString
+});
 
 
 const app = express()
@@ -17,19 +22,19 @@ app.set('view engine', 'ejs')
 //app.get('/', (req, res) => res.render('pages/index'));
 
 app.get('/', getGenreLists);
-app.get('/filter', filterItems);
+//app.get('/filter', filterItems);
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 
 
 
-function getGenreLists(req, res){
+function getGenreLists(req, res) {
     console.log("building genre lists");
 
-    var sql="SELECT genre_name FROM genre";
+    var sql = "SELECT genre_name, genre_id FROM genre";
 
-    pool.query(sql, function (err, result){
-        if (err){
+    pool.query(sql, function (err, result) {
+        if (err) {
             console.log("unable to generate genre list");
             console.log(err);
             callback(err, null);
@@ -38,17 +43,34 @@ function getGenreLists(req, res){
         var returned = JSON.stringify(result.rows);
         var parsedJSON = JSON.parse(returned);
         var dropdown = [];
-        for (var i=0; i<parsedJSON.length; i++){
+        var ids = []
+        for (var i = 0; i < parsedJSON.length; i++) {
             dropdown.push(parsedJSON[i].genre_name);
+            ids.push(parsedJSON[i].genre_id);
         }
-        res.render('pages/index', {dropdown});
+        res.render('pages/index', {
+            dropdown, ids
+        });
     })
 
 }
 
-function filterItems(req, res) {
-    console.log("made it to filter");
-    var inputValue = req.body.genreFilter;
-    console.log("Input Value" + inputValue);
+/* // */
 
-}
+/* function getFilteredBooks(id, callback) {
+    console.log("getting " + id + "From database");
+
+    var sql = "SELECT b.book_id,b.book_name,b.author_id,b.genre_id,a.author_id,a.author_name FROM book b JOIN author a ON b.author_id = a.author_id WHERE b.genre_id = $1::int";
+    var params = [id];
+
+    pool.query(sql, params, function(err, result){
+        if (err){
+            console.log("An error with the database occurred");
+            console.log(err);
+            callback(err, null);
+        }
+
+        console.log("Found DB result: " + JSON.stringify(result.rows));
+        callback(null, result.rows);
+    })
+} */
