2aabc070b085afc249f68149810f3fdf3b190dcf
jessicasi <jessicasi@byui.edu>
2021-03-10 16:43:28 -0800

    minor changes

---
+++ bibliotekDB.sql
@@ -0,0 +1,132 @@
+CREATE TABLE movie 
+( 
+    movie_id serial NOT NULL Primary Key,
+    movie_name VARCHAR(75) NOT NULL,
+    movie_summary TEXT
+);
+
+
+ALTER TABLE book
+
+ADD COLUMN genre_id INT;
+
+    ADD COLUMN series_id INT,
+    ADD COLUMN review_id INT,
+    ADD COLUMN location_id INT;
+
+ALTER TABLE book
+ADD CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) references genre(genre_id);
+
+
+    ADD CONSTRAINT fk_series_id FOREIGN KEY (series_id) references series(series_id),
+    ADD CONSTRAINT fk_review_id FOREIGN KEY (review_id) references review(review_id),
+    ADD CONSTRAINT fk_location_id FOREIGN KEY (location_id) references location(location_id);
+
+
+
+CREATE TABLE book
+(
+    book_id serial NOT NULL Primary Key,
+    book_name VARCHAR(75) NOT NULL,
+    book_format VARCHAR(20) NOT NULL,
+    book_summary TEXT
+  
+);
+
+ALTER TABLE book
+
+    ADD COLUMN series_id INT,
+    ADD COLUMN review_id INT,
+    ADD COLUMN location_id INT;
+
+ALTER TABLE book
+  ADD CONSTRAINT fk_series_id FOREIGN KEY (series_id) references series(series_id),
+    ADD CONSTRAINT fk_review_id FOREIGN KEY (review_id) references review(review_id),
+    ADD CONSTRAINT fk_location_id FOREIGN KEY (location_id) references location(location_id);
+
+CREATE TABLE location
+(
+    location_id serial NOT NULL Primary Key,
+    location_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE series 
+(
+    series_id serial NOT NULL Primary Key,
+    series_name VARCHAR(100) NOT NULL,
+    series_status boolean NOT NULL
+
+);
+
+ALTER TABLE series
+
+    ADD COLUMN review_id INT;
+
+    ALTER TABLE series
+ADD CONSTRAINT fk_review_id FOREIGN KEY (review_id) references review(review_id);
+
+
+CREATE TABLE book_author
+(
+    ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) references book(book_id),
+    ADD CONSTRAINT fk_author_id FOREIGN KEY (author_id) references author(author_id);
+);
+
+ALTER TABLE book_author
+
+    ADD COLUMN book_id INT,
+    ADD COLUMN author_id INT;
+
+ALTER TABLE book_author
+
+ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) references book(book_id),
+    ADD CONSTRAINT fk_author_id FOREIGN KEY (author_id) references author(author_id);
+
+
+
+book_id INT references book.book_id,
+    author_id INT references author.author_id
+
+CREATE TABLE movie_book_genre
+(
+    
+);
+
+movie_id INT references movie.movie_id,
+    book_id INT references book.book_id,
+    genre_id INT references genre.genre_id
+
+ALTER TABLE movie_book_genre
+
+    ADD COLUMN movie_id INT,
+    ADD COLUMN book_id INT,
+    ADD COLUMN genre_id INT;
+
+
+ALTER TABLE movie_book_genre
+
+ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) references book(book_id),
+ADD CONSTRAINT fk_movie_id FOREIGN KEY (movie_id) references movie(movie_id),
+ADD CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) references genre(genre_id);
+
+
+
+
+CREATE TABLE author
+(
+    author_id serial NOT NULL Primary Key,
+    author_name VARCHAR(75)
+);
+
+CREATE TABLE review
+(
+    review_id serial NOT NULL Primary Key,
+    review_text TEXT NOT NULL
+);
+
+
+CREATE TABLE genre
+(
+    genre_id serial NOT NULL Primary Key,
+    genre_name VARCHAR(25) NOT NULL
+);
--- server.js
+++ server.js
@@ -12,40 +12,8 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => res.render('pages/index'));
 
-
-app.post(function (req, res, next) {
-  next();
-});
-
-
-
-
-// async function () {
-//   let response = await fetch('/math_service')
-//   let responseJson = await response.json()
-//   let fromServer = responseJson.myString
-//   alert(fromServer)
-// }
-// app.get(fetch('/results')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-
-
-//   componentWillMount: function(){
-//     var fromServer = fetch('/results')
-//     .then(function(response) {
-//       return response.json()
-//     })
-//     .then(function(responseJson) {
-
-//       return responseJson.myString
-//     })
-
-//     alert(fromServer);
-
-//   },
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
