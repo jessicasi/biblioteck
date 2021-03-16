7dd04f65107c4d1737f4db3d103b493a835c0f1f
jessicasi <jessicasi@byui.edu>
2021-03-16 12:53:48 -0700

    java

--- public/bibliotek-clientside.js
+++ public/bibliotek-clientside.js
@@ -1,7 +1,12 @@
 function searchByBook(){
     console.log("Searching by book...");
-var book = $("#book").value();
+var book = $("#book").val();
 console.log("Book: " + book);
+
+$.get("/filter", function(data){
+    console.log("Back from the server with: ");
+    console.log(data);
+})
 
 }
 
